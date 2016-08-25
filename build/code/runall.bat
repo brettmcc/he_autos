1b0d6649ac01b70e9bb7038666aed097d6af68b4
brettmcc <brettmcc@users.noreply.github.com>
2016-08-25 14:23:58 -0700

    Add build/code batch file
    batch file not running SAS files properly

---
+++ build/code/runall.bat
@@ -0,0 +1,7 @@
+Start/w "C:\Program Files\SASHome\SASFoundation\9.4\sas.exe" -sysin "auto.sas"
+Start/w C:\Program Files\SASHome\SASFoundation\9.4\sas.exe -sysin head.sas
+Start/w C:\Program Files\SASHome\SASFoundation\9.4\sas.exe -sysin housing.sas
+Start/w C:\Program Files\SASHome\SASFoundation\9.4\sas.exe -sysin incomewealth.sas
+Start/w C:\Program Files\SASHome\SASFoundation\9.4\sas.exe -sysin person.sas
+Start/w C:\Program Files\SASHome\SASFoundation\9.4\sas.exe -sysin merge.sas
+"C:\Program Files (x86)\Stata14\Stata-64.exe" /b do finaltouches.do

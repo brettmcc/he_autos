ecf957cebb3720881cd23881860c8c7d453cb3b9
brettmcc <brettmcc@users.noreply.github.com>
2016-09-01 16:30:25 -0700

    Update runall.bat
    Now runs without error

--- build/code/runall.bat
+++ build/code/runall.bat
@@ -1,7 +1,7 @@
-Start/w "C:\Program Files\SASHome\SASFoundation\9.4\sas.exe" -sysin "auto.sas"
-Start/w C:\Program Files\SASHome\SASFoundation\9.4\sas.exe -sysin head.sas
-Start/w C:\Program Files\SASHome\SASFoundation\9.4\sas.exe -sysin housing.sas
-Start/w C:\Program Files\SASHome\SASFoundation\9.4\sas.exe -sysin incomewealth.sas
-Start/w C:\Program Files\SASHome\SASFoundation\9.4\sas.exe -sysin person.sas
-Start/w C:\Program Files\SASHome\SASFoundation\9.4\sas.exe -sysin merge.sas
-"C:\Program Files (x86)\Stata14\Stata-64.exe" /b do finaltouches.do
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\BM-HE-Autos\build\code\person.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\BM-HE-Autos\build\code\auto.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\BM-HE-Autos\build\code\head.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\BM-HE-Autos\build\code\housing.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\BM-HE-Autos\build\code\incomewealth.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\BM-HE-Autos\build\code\merge.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
+"C:\Program Files (x86)\Stata14\Stata-64.exe" /b do W:\BM-HE-Autos\build\code\finaltouches.do

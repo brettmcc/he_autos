1103091de72d3655b1e848bd2dd790b0c6dfaf28
brettmcc <brettmcc@users.noreply.github.com>
2016-09-06 14:33:36 -0700

    Update runall.bat
    change directory

--- build/code/runall.bat
+++ build/code/runall.bat
@@ -1,7 +1,7 @@
-"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\BM-HE-Autos\build\code\person.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
-"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\BM-HE-Autos\build\code\auto.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
-"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\BM-HE-Autos\build\code\head.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
-"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\BM-HE-Autos\build\code\housing.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
-"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\BM-HE-Autos\build\code\incomewealth.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
-"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\BM-HE-Autos\build\code\merge.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
-"C:\Program Files (x86)\Stata14\Stata-64.exe" /b do W:\BM-HE-Autos\build\code\finaltouches.do
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\he_autos-master\build\code\person.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\he_autos-master\build\code\auto.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\he_autos-master\build\code\head.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\he_autos-master\build\code\housing.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\he_autos-master\build\code\incomewealth.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\he_autos-master\build\code\merge.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
+"C:\Program Files (x86)\Stata14\Stata-64.exe" /b do W:\he_autos-master\build\code\finaltouches.do
